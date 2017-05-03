228242f7b5f44971b3075b5b721569fd8c291efb
arron <191446367@qq.com>
2017-05-03 17:53:55 +0800

    have some problem

--- server.js
+++ server.js
@@ -2,81 +2,113 @@ var express = require('express');
 var app = express();
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var request = require('request');
+var axios = require('axios');
 var PORT = 8090;
 var tokens = require('./config')
 var baseUrl = 'http://lolapi.games-cube.com';
-
+var normalToken = {headers: tokens.TOKEN};
 
 // cors
 app.use(cors());
 
 // all champoions
-app.get('/championList', function(req, res) {
-	request({
-		url: `${baseUrl}/champion`,
-		headers: tokens.TOKEN
-	}, function(err, response, body) {
-		res.send(JSON.parse(body));
-	})
+app.get('/championList', async (req, res) => {
+	try {
+		let url = `${baseUrl}/champion`;
+		let response = await axios.get(url, normalToken);
+		let data = response.data;
+		res.send(data);
+	}catch(e) {
+		res.send(e);
+	}
 });
 
 // week free champions
-app.get('/free', function(req, res) {
-	request({
-		url: `${baseUrl}/free`,
-		headers: tokens.TOKEN
-	}, function(err, response, body) {
-		res.send(JSON.parse(body));
-	})
+app.get('/free', async (req, res) => {
+	try {
+		let url = `${baseUrl}/free`;
+		let response = await axios.get(url, normalToken);
+		let data = response.data;
+		res.send(data);
+	}catch(e) {
+		res.send(e);
+	}
 });
 
 // single champion detail
-app.get('/champion/:id', function(req, res) {
-	var championId = req.params.id;
-	request({
-		url: `${baseUrl}/GetChampionDetail?champion_id=${championId}`,
-		headers: tokens.TOKEN
-	}, function(err, response, body) {
-		res.send(JSON.parse(body));
-	})
+app.get('/champion/:id', async (req, res) => {
+	try {
+		let championId = req.params.id;
+		let url = `${baseUrl}/GetChampionDetail?champion_id=${championId}`;
+		let response = await axios.get(url, normalToken);
+		let data = response.data;
+		res.send(data);
+	}catch(e) {
+		res.send(e);
+	}
 });
 
 // ----------------------  // 
 
 
-// player search     /playerresult?key=
-app.get('/playerresult', function(req, res) {
-	var key = String(req.query.key);
-	request({
-		url: `${baseUrl}/UserArea?keyword=${key}`,
-		headers: tokens.TOKEN
-	}, function(err, response, body) {
-		// res.send(JSON.parse(body));
-		res.send(JSON.parse(body))
-	})
+// player search     /playerresult?key=   这边有个中文乱码的问题
+app.get('/playerresult', async (req, res) => {
+	try {
+		let key = String(req.query.key);
+		let url = `${baseUrl}/UserArea?keyword=${key}`;
+		let response = await axios.get(url, normalToken);
+		let data = response.data;
+		res.send(data);
+	}catch(e) {
+		res.send(e);
+	}
 });
 
 // player detail
-app.get('/playerDetail/:qquin/:vaid', function(req, res) {
+app.get('/playerDetail/:qquin/:vaid', async (req, res) => {
+	try {
+		// let qquin = req.params.qquin;
+		// let vaid = req.params.vaid;
+		var result = {};
+		console.log('haha')
+		res.send({name: 'arron'});
+		console.log('bbb')
+
+		// let url = `${baseUrl}/UserHotInfo?qquin=${qquin}&vaid=${vaid}`;
+		// let response = await axios.get(url, normalToken);
+		// let resdata = response.data;
+		// result = resdata;
+		
+		// console.log(result.data[0].tier, 'hhhhh')
+		// url = `${baseUrl}/GetTierQueue?tier=${result.data[0].tier}&queue=${result.data[0].queue}`;
+		// response = await axios.get(url, normalToken);
+		// data = response.data;
+		// result.data[0].tier = data.data[0].return;
+		// url = `${baseUrl}/UserExtInfo?qquin=${qquin}&vaid=${vaid}`;
+		// response = await axios.get(url, normalToken);
+		// data = response.data;
+		// result.data[0].tripleKills = data[1].triple_kills;
+		// result.data[0].quadraKills = data[1].quadra_kills;
+		// result.data[0].pentaKills = data[1].penta_kills;
+		// result.data[0].godlikeNum = data[1].god_like_num;
+		// result.data[0].killsTotal = data[1].kills_total;
+		// result.data[0].totalMvps = data[2].total_match_mvps + data[2].total_rank_mvps;
+		// res.send(result);
+	}catch(e) {
+		console.log(e);
+	}
+});
+
+// combat_list
+app.get('/combatList/:qquin/:vaid/:p', function(req, res) {
 	var qquin = req.params.qquin;
 	var vaid = req.params.vaid;
-	var result = {};
+	var p = req.params.p;
 	request({
-		url: `${baseUrl}/UserHotInfo?qquin=${qquin}&vaid=${vaid}`,
+		url: `${baseUrl}/CombatList?qquin=${qquin}&vaid=${vaid}&p=${p}`,
 		headers: tokens.TOKEN
 	}, function(err, response, body) {
-		result = JSON.parse(body);
-		request({
-			url: `${baseUrl}/GetTierQueue?tier=${result.data[0].tier}&queue=${result.data[0].queue}`,
-			headers: tokens.TOKEN
-		}, function(err, response, body) {
-			var data = JSON.parse(body);
-			if(data.code == 0) {
-				result.data[0].tier = data.data[0].return;
-			}
-			res.send(result);
-		})
+		res.send(JSON.parse(body));
 	})
 });
 
