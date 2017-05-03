034ad497a521eb51f8efbef1e785b7a82de61eff
arron <191446367@qq.com>
2017-05-03 13:43:39 +0800

    remove node_modules

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ var tokens = require('./config')
 var baseUrl = 'http://lolapi.games-cube.com';
 
 
+// cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // all champoions
 app.get('/championList', function(req, res) {
 	request({
